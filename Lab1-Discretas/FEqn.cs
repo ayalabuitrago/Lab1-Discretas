041e3681763b8e3307f383ec73fc04739b23a364
Emanuel Sánchez <emmanueelsanchess79@gmail.com>
2022-08-20 15:22:16 -0600

    Duda

--- Lab1-Discretas/FEqn.cs
+++ Lab1-Discretas/FEqn.cs
@@ -15,6 +15,41 @@ namespace Lab1_Discretas
         public FEqn()
         {
             InitializeComponent();
+            LbR.Items.Add("Las ecuaciones son equivalentes");
+        }
+        //int OpcControl;
+
+        private void BTNeq1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            // OpcControl = 1;
+            LbR.Items.Clear();
+            LbR.Items.Add("Para que x sea equivalente en esta ecuacion debe de tomar los siguientes valores");
+            for (int i = 0; i < 10; i += 2)
+            {
+                LbR.Items.Add(i.ToString() + "\t " + (i + 1).ToString() + "\t");
+
+            }
+
+            for (int j = 21; j < 100; j += 2)
+            {
+                LbR.Items.Add(j.ToString() + "\t " + (j + 1).ToString() + "\t");
+            }
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //OpcControl = 2;
+            LbR.Items.Clear();
+
+                LbR.Items.Add("Para que x sea equivalente en esta ecuacion su valor no debe ser ninguno de estos");
+                for (int k = 10; k < 20; k++)
+                {
+                    LbR.Items.Add(k.ToString() + "\t" + (k + 1).ToString() + "\t");
+                }
+
+            
         }
     }
 }
