7b950d067f0954f56389ace0366aa835b6fd9d94
ayalabuitrago <ayalabuitrago@gmail.com>
2022-08-20 10:32:03 -0600

    Agregar archivos de proyecto.

---
+++ Lab1-Discretas/main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_Discretas
+{
+    public partial class Main : Form
+    {
+        
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BTNOpenEqn_Click(object sender, EventArgs e)
+        {
+            Form eqn = new FEqn();
+            eqn.Show();
+        }
+
+        private void BTNopenTrueTable_Click(object sender, EventArgs e)
+        {
+            Form trueTable = new FTrueTable();
+            trueTable.Show();
+
+        }
+    }
+}
